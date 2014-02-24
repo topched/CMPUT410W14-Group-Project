e1984f1c328215658f29a9a2ea65d06bbd0d94b8
Torboto <motifo@gmail.com>
2014-02-24 13:09:11 -0700

    Fixing .urls files

--- mynode/app/urls.py
+++ mynode/app/urls.py
@@ -3,7 +3,8 @@ from django.conf.urls import patterns, url
 #from app import views
 
 urlpatterns = patterns('',
-        url(r'^feed/', include('feed.urls')),
-        url(r'^friends/', include('friends.urls')),
+        # ex: /feed/
+        url(r'^feed/', views.feed, name='feed'),
+        url(r'^friends/', views.friends, name='friends'),
         url(r'^admin/', include(admin.site.urls)),
 )
