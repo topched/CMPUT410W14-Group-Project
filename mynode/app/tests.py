f02c65d076c775792bf7b3b6b0bd18c1cbdcfb6b
Kris Kushniruk <kkushnir@ualberta.ca>
2014-03-10 13:35:37 -0600

    Some more tests

--- mynode/app/tests.py
+++ mynode/app/tests.py
@@ -44,94 +44,137 @@ class testRunner(TestCase):
     def setUp(self):
         self.factory = RequestFactory()
         self.user = User.objects.create_user(
-    		username="admin",
-    		email="test@test.com",
-    		password="password",
-    		first_name="Admin",
-    		last_name="Person"
-    		)
+            username="admin",
+            email="test@test.com",
+            password="password",
+            first_name="Admin",
+            last_name="Person")
+
         self.app_user = Users.objects.create(user = self.user, git_url="test.git")
 
         self.post = Post.objects.create(author = self.user, content="My first post")
         self.post_id = self.post.id
 
+    def test_create_user(self):
 
-    def test_get_stream(self):
+        resp = self.client.get('/mynode/register', follow=True)
+        self.assertEqual(resp.status_code, 200)
 
-    	#No logged in user - should redirect to login
-    	resp = self.client.get('/mynode/stream/', follow=True)
-    	self.assertRedirects(resp, '/mynode/')
+        tmp = User.objects.all()
+        start = len(tmp)
 
+        resp = self.client.post(
+            '/mynode/register/',
+            {'username':'someUser',
+            'email':'fake@fake.com',
+            'pwd':'password',
+            'surname':'some',
+            'lastname':'user',
+            'git':'fake.git'})
 
-        #Logged user should return the stream page
-    	self.client.login(username='admin', password='password')
-    	resp = self.client.get('/mynode/stream/')
-    	self.assertEqual(resp.status_code, 200)
-    	self.assertTemplateUsed(resp, 'stream_page.html')
+        tmp = User.objects.all()
+        end = len(tmp)
+
+        self.assertEqual(end, start+1)
+        self.assertEqual(tmp[end-1].username,'someUser')
+
 
     def test_create_post(self):
 
-    	posts = Post.objects.all()
-    	self.client.login(username='admin',password='password')
+        posts = Post.objects.all()
+        self.client.login(username='admin',password='password')
 
-    	resp = self.client.post(
-    	    '/mynode/stream/post/create/', 
-    	    {'content':'My second post'}
-    	)
+        resp = self.client.post(
+            '/mynode/stream/post/create/', 
+            {'content':'My second post'}
+        )
 
-    	tmp = Post.objects.all()
-    	
-    	self.assertEqual(len(tmp),2)
-    	self.assertEqual(tmp[1].content, "My second post")
+        tmp = Post.objects.all()
+    
+        self.assertEqual(len(tmp),2)
+        self.assertEqual(tmp[1].content, "My second post")
 
     def test_delete_post(self):
 
-    	PostToDelete = Post.objects.create(author=self.user,content="A post to delete")
-    	self.client.login(username='admin',password='password')
+        PostToDelete = Post.objects.create(author=self.user,content="A post to delete")
+        self.client.login(username='admin',password='password')
 
-    	posts = Post.objects.all()
-    	start = len(posts)
+        posts = Post.objects.all()
+        start = len(posts)
 
-    	url = '/mynode/stream/post/' + str(PostToDelete.id) + '/delete/'
+        url = '/mynode/stream/post/' + str(PostToDelete.id) + '/delete/'
 
-    	resp = self.client.post(url)
-    	tmp = Post.objects.all()
+        resp = self.client.post(url)
+        tmp = Post.objects.all()
     	
-    	self.assertEqual(len(tmp), start-1)
-    	
+        self.assertEqual(len(tmp), start-1)
+
+    def test_create_comment(self):
+
+        self.client.login(username='admin',password='password')
+
+        tmp = Comment.objects.all()
+        start = len(tmp)
+
+        url = '/mynode/stream/post/' + str(self.post_id) + '/comments/'
+
+        resp = self.client.post(
+            url,
+            {'parent_post':self.post_id, 'author':self.user, 'content':'My first comment'})
+
+        tmp = Comment.objects.all()
+        end = len(tmp)
+        self.assertEqual(start+1,end)
+
+        val = tmp[end-1].content
+        self.assertEqual(val, 'My first comment')
+
+    def test_get_stream(self):
+
+        #No logged in user - should redirect to login
+        resp = self.client.get('/mynode/stream/', follow=True)
+        self.assertRedirects(resp, '/mynode/')
+
+
+        #Logged user should return the stream page
+        self.client.login(username='admin', password='password')
+        resp = self.client.get('/mynode/stream/')
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, 'stream_page.html')
+
     def test_get_profile(self):
 
-    	#No logged in user - should redirect
-    	resp = self.client.get('/mynode/profile/', follow=True)
-    	self.assertRedirects(resp, '/mynode/')
+        #No logged in user - should redirect
+        resp = self.client.get('/mynode/profile/', follow=True)
+        self.assertRedirects(resp, '/mynode/')
 
-    	#Logged in user should return profile page
-    	self.client.login(username='admin', password='password')
-    	resp = self.client.get('/mynode/profile/')
-    	self.assertEqual(resp.status_code, 200)
-    	self.assertTemplateUsed(resp, 'profile_page.html')
+        #Logged in user should return profile page
+        self.client.login(username='admin', password='password')
+        resp = self.client.get('/mynode/profile/')
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, 'profile_page.html')
     	
     def test_get_friends(self):
 
-    	#No logged in user - should redirect
-    	resp = self.client.get('/mynode/friends/', follow=True)
-    	self.assertRedirects(resp, '/mynode/')
+        #No logged in user - should redirect
+        resp = self.client.get('/mynode/friends/', follow=True)
+        self.assertRedirects(resp, '/mynode/')
 
-    	#Logged in user should return friends page
-    	self.client.login(username='admin', password='password')
-    	resp = self.client.get('/mynode/friends/')
-    	self.assertEqual(resp.status_code, 200)
-    	self.assertTemplateUsed(resp, 'friend_page.html')
+        #Logged in user should return friends page
+        self.client.login(username='admin', password='password')
+        resp = self.client.get('/mynode/friends/')
+        self.assertEqual(resp.status_code, 200)
+        self.assertTemplateUsed(resp, 'friend_page.html')
 
     def test_get_admin(self):
 
         #Logged in user
-    	self.client.login(username='admin', password='password')
-    	resp = self.client.get('/admin/')
-    	self.assertEqual(resp.status_code, 200)
+        self.client.login(username='admin', password='password')
+        resp = self.client.get('/admin/')
+        self.assertEqual(resp.status_code, 200)
 
     def test_root_redirect(self):
 
     	#General root redirection
-    	resp = self.client.get('/')
+        resp = self.client.get('/')
         self.assertRedirects(resp, '/mynode/')
