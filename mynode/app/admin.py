13e273a06061eb56b8f045d737ac2f64f95fffee
christianjukna <jukna@ualberta.ca>
2014-03-09 22:28:43 -0600

    Added Filtering to Admin Panel and basic tests

--- mynode/app/admin.py
+++ mynode/app/admin.py
@@ -4,17 +4,10 @@ from django.conf.urls import patterns
 from django.shortcuts import render_to_response
 from app.models import *
 
-#class ApproveUsersInline(admin.StackedInline):
-#    model = models.OneToOneField(Users)
-#    max_num = 1
-#    can_delete = False
-
-#class ApproveUsers(admin.ModelAdmin):
-#    inlines = [ApproveUsersInline]
-#    list_display = ('user', 'approved')
-#    def queryset(self, request):
-#        qs = super(ApproveUsers, self).queryset(request)
-#        return qs.filter(approved=0)
+#Admin approval action method for approving users
+def approve(modeladmin, request, queryset):
+    queryset.update(approved=1)
+approve.short_description = "Approve the selected Users"
 
 class Posts(admin.ModelAdmin):
     list_display = ('id', 'author', 'content', 'content_type', 'visibility')
@@ -27,6 +20,8 @@ class Friends(admin.ModelAdmin):
 
 class Users_Admin(admin.ModelAdmin):
     list_display = ('user', 'git_url', 'default_post_visibility', 'approved')
+    list_filter = ['approved']
+    actions = [approve]
 
 admin.site.register(Comment,Comments)
 admin.site.register(Friend,Friends)
