fbc9f6f1499762b5e8fad9ad5503126952156765
Kris Kushniruk <kkushnir@ualberta.ca>
2014-03-10 12:49:45 -0600

    Added some tests, have a few more in the works

--- mynode/app/tests.py
+++ mynode/app/tests.py
@@ -34,3 +34,101 @@ class GetTests(unittest.TestCase):
 
         response = self.c.get('/mynode/friends/')
         self.assertEqual(response.status_code, 200)
+
+#Looks like we had a few of the same tests Christian
+class testRunner(TestCase):
+    
+    def setUp(self):
+        self.factory = RequestFactory()
+        self.user = User.objects.create_user(
+    		username="admin",
+    		email="test@test.com",
+    		password="password",
+    		first_name="Admin",
+    		last_name="Person"
+    		)
+        self.app_user = Users.objects.create(user = self.user, git_url="test.git")
+
+        self.post = Post.objects.create(author = self.user, content="My first post")
+        self.post_id = self.post.id
+
+
+    def test_get_stream(self):
+
+    	#No logged in user - should redirect to login
+    	resp = self.client.get('/mynode/stream/', follow=True)
+    	self.assertRedirects(resp, '/mynode/')
+
+
+        #Logged user should return the stream page
+    	self.client.login(username='admin', password='password')
+    	resp = self.client.get('/mynode/stream/')
+    	self.assertEqual(resp.status_code, 200)
+    	self.assertTemplateUsed(resp, 'stream_page.html')
+
+    def test_create_post(self):
+
+    	posts = Post.objects.all()
+    	self.client.login(username='admin',password='password')
+
+    	resp = self.client.post(
+    	    '/mynode/stream/post/create/', 
+    	    {'content':'My second post'}
+    	)
+
+    	tmp = Post.objects.all()
+    	
+    	self.assertEqual(len(tmp),2)
+    	self.assertEqual(tmp[1].content, "My second post")
+
+    def test_delete_post(self):
+
+    	PostToDelete = Post.objects.create(author=self.user,content="A post to delete")
+    	self.client.login(username='admin',password='password')
+
+    	posts = Post.objects.all()
+    	start = len(posts)
+
+    	url = '/mynode/stream/post/' + str(PostToDelete.id) + '/delete/'
+
+    	resp = self.client.post(url)
+    	tmp = Post.objects.all()
+    	
+    	self.assertEqual(len(tmp), start-1)
+    	
+    def test_get_profile(self):
+
+    	#No logged in user - should redirect
+    	resp = self.client.get('/mynode/profile/', follow=True)
+    	self.assertRedirects(resp, '/mynode/')
+
+    	#Logged in user should return profile page
+    	self.client.login(username='admin', password='password')
+    	resp = self.client.get('/mynode/profile/')
+    	self.assertEqual(resp.status_code, 200)
+    	self.assertTemplateUsed(resp, 'profile_page.html')
+    	
+    def test_get_friends(self):
+
+    	#No logged in user - should redirect
+    	resp = self.client.get('/mynode/friends/', follow=True)
+    	self.assertRedirects(resp, '/mynode/')
+
+    	#Logged in user should return friends page
+    	self.client.login(username='admin', password='password')
+    	resp = self.client.get('/mynode/friends/')
+    	self.assertEqual(resp.status_code, 200)
+    	self.assertTemplateUsed(resp, 'friend_page.html')
+
+    def test_get_admin(self):
+
+        #Logged in user
+    	self.client.login(username='admin', password='password')
+    	resp = self.client.get('/admin/')
+    	self.assertEqual(resp.status_code, 200)
+
+    def test_root_redirect(self):
+
+    	#General root redirection
+    	resp = self.client.get('/')
+        self.assertRedirects(resp, '/mynode/')
