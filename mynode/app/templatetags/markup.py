2492421e4c22e253081c31ff8af4173c982f3588
Jordan Ching <ching.jordan@gmail.com>
2014-03-25 22:03:54 -0600

    Template filter files added

---
+++ mynode/app/templatetags/markup.py
@@ -0,0 +1,12 @@
+import markdown2
+from django import template
+from django.template.defaultfilters import stringfilter
+from django.utils.encoding import force_unicode
+from django.utils.safestring import mark_safe
+
+register = template.Library()
+
+@register.filter(name='convert_markdown')
+@stringfilter
+def convert_markdown(value):
+    return mark_safe(markdown2.markdown(force_unicode(value),safe_mode=True))
