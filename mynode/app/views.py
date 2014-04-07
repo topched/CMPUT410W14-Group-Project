34e8b0d698141be3cbb2a1dc6bf3e95de32004dd
Torboto <motifo@gmail.com>
2014-04-07 00:42:07 -0600

    Merged our three post pages into a tabbed interface on the stream page. They still work, don't worry. This was done in a very hacky way because the forms are generated with django in the backend, so I use jquery/ajax to grab the html page, parse out the form, and insert it into a div.

--- mynode/app/views.py
+++ mynode/app/views.py
@@ -1,3 +1,17 @@
+#    Copyright 2014 Christian Jukna, Erin Torbiak, Jordan Ching, Kris Kushniruk
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
 from django.shortcuts import render
 from django.shortcuts import render_to_response
 from django.shortcuts import redirect
@@ -211,10 +225,10 @@ def delete_post(request, post_id):
     #TODO: Should delete all related COMMENTS as well
     return redirect('app.views.stream')
 
-
 def create_post(request, post_id=None):
     if request.method == 'GET':
         postForm = PostForm(user=request.user.id)
+
         return render(request, 'create_post.html', {'PostForm': postForm})
     else:
         current_user = User.objects.get(id=request.user.id)
