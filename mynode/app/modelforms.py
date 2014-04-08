fe551ae1ca2e7aed65805db7073a575d86b02e98
Torboto <motifo@gmail.com>
2014-04-07 21:34:49 -0600

    Advanced posts now look much better, and work! For me at least.

--- mynode/app/modelforms.py
+++ mynode/app/modelforms.py
@@ -11,8 +11,11 @@ class ImageForm(ModelForm):
         exclude = ('author',)
 
 class PostForm(ModelForm):
-    title = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'rows':1}))
-    content = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'rows':4}))
+    title = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'rows':1, 'placeholder':'Title'}))
+    content = forms.CharField(widget=forms.Textarea(attrs={'class': 'form-control', 'rows':3, 'placeholder':'Content...'}))
+    visibility = forms.ChoiceField(choices=Post.VISIBILITY_CHOICES, widget=forms.Select(attrs={'class': 'form-control', 'style':'width:150px;',}))
+    content_type = forms.ChoiceField(choices=Post.CONTENT_CHOICES, widget=forms.Select(attrs={'class': 'form-control', 'style':'width:150px;',}))
+
     class Meta:
         model = Post
         exclude = ('author',)
@@ -20,5 +23,7 @@ class PostForm(ModelForm):
     def __init__(self, *args, **kwargs):
         user = kwargs.pop('user','')
         super(PostForm, self).__init__(*args, **kwargs)
-        self.fields['recipient']=forms.ModelChoiceField(queryset=User.objects.exclude(id=user).exclude(is_superuser=1), required=False)
-        self.fields['image']=forms.ModelChoiceField(queryset=Image.objects.filter(author=user), required=False)
+        self.fields['recipient']=forms.ModelChoiceField(queryset=User.objects.exclude(id=user).exclude(is_superuser=1), required=False, widget=forms.Select(attrs={'class': 'form-control', 'style':'width:150px;',}))
+        self.fields['image']=forms.ModelChoiceField(queryset=Image.objects.filter(author=user), required=False, widget=forms.Select(attrs={'class': 'form-control', 'style':'width:150px;',}))
+
+        
